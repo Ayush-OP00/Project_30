e97248a35b6ad082da479cb3a3fe7b82e6b6e08c
Ayush-OP00 <allinone7925@gmail.com>
2026-03-30 15:36:31 +0530

    Created Profile model for storing additional user details

--- project30/projectApp/models.py
+++ project30/projectApp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    # Extra Fields
+    name = models.CharField(max_length=100)
+    mobile = models.CharField(max_length=10)
+    dob = models.DateField(null=True, blank=True)
+
+    GENDER_CHOICES = (
+        ('male', 'Male'),
+        ('female', 'Female'),
+        ('other', 'Other'),
+    )
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES, blank=True)
+
+    address = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.user.username
+
